d446921b8c0a10251f769da2c0684efbbc2bef95
meseeks <nizam.alesevic@gmail.com>
2019-10-14 23:50:39 +0200

    mobile friendly

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 
   var app = $.spapp({pageNotFound : 'error_404'}); // initialize
   // define routes
-  app.route({view: '/', load: 'home.html' });
   app.route({view: 'home', load: 'home.html' });
   app.route({view: 'projects', load: 'projects.html' });
   // run app
